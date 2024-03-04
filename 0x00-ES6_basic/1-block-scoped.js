36fe09d144918401847017bde4be4e65b26c08be
Amanbel <amanuel11belachew@gmail.com>
2024-03-05 00:37:26 +0300

    es6

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true; // eslint-disable-line
 
   if (trueOrFalse) {
-    let task = true; // eslint-disable-line
-    let task2 = false; // eslint-disable-line
+    const task = true; // eslint-disable-line
+    const task2 = false; // eslint-disable-line
   }
 
   return [task, task2];
