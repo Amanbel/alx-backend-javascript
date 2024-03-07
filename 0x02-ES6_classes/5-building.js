af1495b54860bc8b9bf1869f1b163c403c5fd302
Amanbel <amanuel11belachew@gmail.com>
2024-03-08 01:56:31 +0300

    classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,11 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor.evacuationWarningMessage) {
-      this._sqft = sqft;
-    } else {
-      throw Error('Class extending Building must override evacuationWarningMessage');
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage() !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
     }
+    this._sqft = sqft;
   }
 
   get sqft() {
