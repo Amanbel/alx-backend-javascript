eb0beefb49ee5a4e065b0e25c1e452d02dfbca4c
Amanbel <amanuel11belachew@gmail.com>
2024-03-09 01:26:18 +0300

    classes

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -4,7 +4,23 @@ export default class Airport {
     this._code = code;
   }
 
-  toString() {
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
     return this._code;
   }
 }
