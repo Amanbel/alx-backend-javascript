7424636b18c0ba2b2e06a963d885d58600242f23
Amanbel <amanuel11belachew@gmail.com>
2024-03-09 01:14:34 +0300

    classes

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,8 +2,7 @@ import Building from './5-building.js'; // eslint-disable-line
 
 export default class skyHighBuilding extends Building {
   constructor(sqft, floors) {
-    super();
-    super.sqft = sqft;
+    super(sqft);
     this._floors = floors;
   }
 
