82e77b7988fdc29c9c979a106bdf288db4b0f770
Amanbel <amanuel11belachew@gmail.com>
2024-03-08 23:23:47 +0300

    promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils.js'; // eslint-disable-line
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then(([result1, result2]) => {
       console.log(`${result1.body} ${result2.firstName} ${result2.lastName}`);
     })
