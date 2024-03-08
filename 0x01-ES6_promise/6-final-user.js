24d54e319a7296d249d6be957881d490b5842f66
Amanbel <amanuel11belachew@gmail.com>
2024-03-08 23:51:24 +0300

    promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,13 @@
 import signUpUser from './4-user-promise.js' // eslint-disable-line
 import uploadPhoto from './5-photo-reject.js' // eslint-disable-line
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
       res.map((o) => ({
         status: o.status,
         value: o.status === 'fulfilled' ? o.value : String(o.reason),
-      }));
-    });
+      }))
+    ));
 }
