bb8d6d0e394a225b2f23a7fd2490d38630725a46
Amanbel <amanuel11belachew@gmail>
2024-04-22 13:42:48 +0300

    3-fix

--- 0x03-ES6_data_manipulation/3-get_ids_num.js
+++ 0x03-ES6_data_manipulation/3-get_ids_num.js
@@ -1,5 +1,3 @@
 export default function getStudentIdsSum(students) {
-  return students.iterable.reduce((previous, current) => {
-    return previous + current.id
-  }, 0)
+  return students.reduce((previous, current) => previous + current.id, 0)
 }
