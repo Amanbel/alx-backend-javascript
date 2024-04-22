d7ab865303681f6e4ad1f1457fdcc00a0845ca89
Amanbel <amanuel11belachew@gmail>
2024-04-22 15:27:21 +0300

    8-fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,20 @@
 export default function cleanSet(set, startString) {
-  const holderArray = [];
-  set.forEach((item) => {
-    if (item.includes(startString)) {
-      holderArray.push(item.replace(startString, ''));
-    }
-  });
+  if (
+    !set && !startString && !(set instanceof Set) && typeof startString !== 'string'
+  ) {
+    return '';
+  }
 
-  return holderArray.join('-');
+  const parts = [];
+
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
 }
