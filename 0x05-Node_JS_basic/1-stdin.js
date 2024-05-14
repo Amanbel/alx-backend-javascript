431a0ad8b2bbe9fe87d4b5aa39a40feaf1c147dd
Amanbel <amanuel11belachew@gmail>
2024-05-14 22:10:25 +0300

    2-read

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,10 @@
-const readline = require('node:readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
-rl.question('Welcome to Halberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  rl.on('close', () => {
-    console.log('This important software is now closing');
-  });
-  rl.close();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
 });
